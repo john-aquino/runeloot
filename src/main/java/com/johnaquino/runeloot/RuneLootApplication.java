6abb7dcf692a6cbfb92fe07ca01abe3128cdb5ce
John Aquino <johnaquino@outlook.com>
2024-06-28 23:02:12 -0400

    Create repo

---
+++ src/main/java/com/johnaquino/runeloot/RuneLootApplication.java
@@ -0,0 +1,13 @@
+package com.johnaquino.runeloot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuneLootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuneLootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnaquino/runeloot/RuneLootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnaquino.runeloot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuneLootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
